19006584d3f2c57f545a18ef97048c90446639c7
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2024-10-03 12:30:00 -0400

    Add alchemy, enchanting, and blacksmithing skills

--- player.py
+++ player.py
@@ -72,7 +72,11 @@ class Player(pygame.sprite.Sprite):
         # Combat mechanics
         self.crit_chance = 0.1
         self.block_chance = 0.15            self.direction.x = 1
-        elif keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        
+        # Professions
+        self.alchemy_level = 1
+        self.enchanting_level = 1
+        self.blacksmithing_level = 1        elif keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.direction.x = -1
         else:
             self.direction.x = 0
